680a7361bb290cc4305abcb1c234181fb97f84c9
battlecoder <ezacarias@gmail.com>
2023-11-19 04:10:56 -0300

    Added second enemy type

--- Scripts/Enemy2.cs
+++ Scripts/Enemy2.cs
@@ -1,11 +1,12 @@
 using Godot;
 using System;
 
-public class Enemy1 : BaseEnemy {
-    
+public class Enemy2 : BaseEnemy {
+    float tTime = 0;
     public override void DoMovement(float delta) {
+        tTime += delta;
         // Very basic motion. Keep our velocity constant, to the left, as long as we
         // are inside the screen
-        this.velocity = new Vector2(-200, 0);
+        this.velocity = new Vector2(-200, 150*((float)Math.Sin(tTime*5)));
     }
 }
